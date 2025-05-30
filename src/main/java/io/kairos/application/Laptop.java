31e97a010515d11fcc7815d649e0aef0d34f3d2c
Chidera Mosanya <deramosanya@gmail.com>
2025-05-30 09:38:36 -0400

    experimenting with autowired and component of springboot framework

---
+++ src/main/java/io/kairos/application/Laptop.java
@@ -0,0 +1,10 @@
+package io.kairos.application;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+    public void compile() {
+        System.out.println("Compile");
+    }
+}
--- src/test/java/io/kairos/application/SpringBootDemoApplicationTests.java
+++ src/test/java/io/kairos/application/SpringBootDemoApplicationTests.java
@@ -1,4 +1,4 @@
-package io.kairos.applications;
+package io.kairos.application;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
