43a55808f99dd9c58a512323ce7fcaa9dd96e26d
Chidera Mosanya <deramosanya@gmail.com>
2025-05-30 21:48:15 -0400

    created data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into student (rollno, name, marks) values (101, "Chidera", 72);
+insert into student (rollno, name, marks) values (102, "Naza", 76);
+insert into student (rollno, name, marks) values (103, "Static", 82);
