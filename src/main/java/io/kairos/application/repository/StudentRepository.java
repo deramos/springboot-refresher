4922d10becb44bb163b5f58753c7dd4151aeef5c
Chidera Mosanya <deramosanya@gmail.com>
2025-05-30 21:18:03 -0400

    created schema.sql

--- src/main/java/io/kairos/application/repository/StudentRepository.java
+++ src/main/java/io/kairos/application/repository/StudentRepository.java
@@ -2,6 +2,8 @@ package io.kairos.application.repository;
 
 
 import io.kairos.application.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -9,9 +11,22 @@ import java.util.List;
 
 @Repository
 public class StudentRepository {
+    private JdbcTemplate jdbc;
+
+    public JdbcTemplate getJdbc() {
+        return jdbc;
+    }
+
+    @Autowired
+    public void setJdbc(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
 
     public void save(Student student) {
-        System.out.println("Student Saved");
+        String sql = "insert into student (rollno, name, marks) values(?, ?, ?)";
+        int rows = jdbc.update(sql, student.getRollNo(), student.getName(), student.getMarks());
+        System.out.println(rows + " affected");
+
     }
 
     public List<Student> findAll() {
