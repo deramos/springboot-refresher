b5cf11a96e314d732f3a502a8940cc3e68551be5
Chidera Mosanya <deramosanya@gmail.com>
2025-05-30 20:09:40 -0400

    added student repository

--- src/main/java/io/kairos/application/service/StudentService.java
+++ src/main/java/io/kairos/application/service/StudentService.java
@@ -1,10 +1,23 @@
 package io.kairos.application.service;
 
 import io.kairos.application.model.Student;
+import io.kairos.application.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
+
+    private StudentRepository studentRepository;
+
+    public StudentRepository getStudentRepository() {
+        return studentRepository;
+    }
+
+    @Autowired
+    public void setStudentRepository(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
 
     public void addStudent(Student student){
         System.out.println("Student added");
