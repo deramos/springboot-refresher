78cbbb24ff3bba8a7cee518d81d08a0ff91738bd
Chidera Mosanya <deramosanya@gmail.com>
2025-06-05 11:58:42 -0400

    added aspect logging

--- src/main/java/io/kairos/application/aop/LoggingAspect.java
+++ src/main/java/io/kairos/application/aop/LoggingAspect.java
@@ -1,4 +1,18 @@
 package io.kairos.application.aop;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
 public class LoggingAspect {
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
+
+    @Before("")
+    public void logMethodCal() {
+        LOGGER.info("Service Called");
+    }
 }
