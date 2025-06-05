a7820c8abb66e95fb4394461b32d77b44ffb30f8
Chidera Mosanya <deramosanya@gmail.com>
2025-06-05 14:54:20 -0400

    added after throw and after

--- src/main/java/io/kairos/application/aop/LoggingAspect.java
+++ src/main/java/io/kairos/application/aop/LoggingAspect.java
@@ -1,5 +1,8 @@
 package io.kairos.application.aop;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -12,7 +15,20 @@ public class LoggingAspect {
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
 
     @Before("execution(* io.kairos.application.service.StudentService.*(..))")
-    public void logMethodCal() {
+    public void logMethodCall(JoinPoint point) {
+        System.out.println(point.getSourceLocation() + ": " + point.getSignature().getName());
         LOGGER.info("Service Called");
+    }
+
+    @After("execution(* io.kairos.application.service.StudentService.*(..))")
+    public void executeMethodCall(JoinPoint point) {
+        System.out.println(point.getSourceLocation() + ": " + point.getSignature().getName());
+        LOGGER.info("After Service Called");
+    }
+
+    @AfterThrowing("execution(* io.kairos.application.service.StudentService.*(..))")
+    public void logExceptionThrow(JoinPoint point) {
+        System.out.println(point.getSourceLocation() + ": " + point.getSignature().getName());
+        LOGGER.info("Throw after Exception Call");
     }
 }
